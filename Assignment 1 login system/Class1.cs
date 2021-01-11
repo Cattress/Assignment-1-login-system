d1140d252098228605340e45802ecd690dee9d0a
Cattress <priyaphon.vincent@gmail.com>
2021-01-11 12:15:49 +0700

    Add project files.

---
+++ Assignment 1 login system/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_1_login_system
+{
+    class user
+    {
+        public string realName; //public == any can
+        private string username; //private == other programs cannot access
+        private string password;
+        public bool loggedIn = false;
+
+        public user(string un, string pw, string rln) //constructor
+        {
+            username = un;
+            password = pw;
+            realName = rln;
+        }
+        public bool checkLogin(string un, string pw) //function to check their username and password
+        {
+            if (un == username && pw == password)
+
+            {
+
+                loggedIn = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+           public void logOut()
+        {
+            loggedIn = false;
+        
+        }
+        public void changePassword(string newPassword)
+        {
+            if(loggedIn == true)
+            {
+                Console.WriteLine("You have changed your password");
+                password = newPassword;
+            }
+            else
+            {
+                Console.WriteLine("you are not logged in. cannot change password!");
+            }
+        }
+    }
+}
---
+++ Assignment 1 login system/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Assignment_1_login_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            user bobby = new user("bob","bob123","Bob Smith");
+            Console.WriteLine("User Logged in: "+bobby.loggedIn);
+
+            Console.Write(bobby.checkLogin("Adam", "adam123"));
+            Console.Write(bobby.checkLogin("Adam", "bob123"));
+            Console.Write(bobby.checkLogin("bob", "bob123"));
+
+            Console.WriteLine("User Logged in: " + bobby.loggedIn);
+
+
+            Console.WriteLine(bobby.realName);
+
+            bobby.logOut();
+            bobby.changePassword("brown123");
+            Console.WriteLine("user logged in: " + bobby.loggedIn);
+        }
+    }
+}
